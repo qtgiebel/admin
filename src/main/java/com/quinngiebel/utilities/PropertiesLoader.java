7a9b7815b1a6e02a812550af4ed43ced78528a0b
qtgiebel <qtgiebel@gmail.com>
2022-02-28 12:25:44 -0600

    update add error logging

--- src/main/java/com/quinngiebel/utilities/PropertiesLoader.java
+++ src/main/java/com/quinngiebel/utilities/PropertiesLoader.java
@@ -2,20 +2,25 @@ package com.quinngiebel.utilities;
 
 import java.util.Properties;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * @author Eric Knapp
  *
  */
 public interface PropertiesLoader {
 
-    /*default Properties loadProperties(String propertiesFilePath) throws Exception {
+    default Properties loadProperties(String propertiesFilePath) throws Exception {
         Properties properties = new Properties();
+        Logger logger = LogManager.getLogger(this.getClass());
+
         try {
             properties.load(this.getClass().getResourceAsStream(propertiesFilePath));
         } catch (Exception ioException) {
-            ioException.printStackTrace();
+            logger.error("Error loading properties...", ioException);
             throw ioException;
         }
         return properties;
-    }*/
+    }
 }
